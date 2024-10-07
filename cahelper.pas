8da2b82803664951833b605173ed1aca8f1081fd
carlca <carl.caulkett@gmail.com>
2024-10-07 23:36:55 +0100

    Working on Settings.

--- cahelper.pas
+++ cahelper.pas
@@ -8,7 +8,7 @@ unit caHelper;
 interface
 
 uses
-  Classes, SysUtils;
+  Classes, SysUtils, TypInfo;
 
 type
 
@@ -22,18 +22,57 @@ type
     function Pred: int64;
   end;
 
+  TcaShiftStateHelper = type helper for TShiftState
+  private
+    function ShiftToString(shift: TShiftState): string;
+  public
+    function ToString: string;
+  end;
+
 implementation
+
+// TcaIntegerHelper
 
 function TcaIntegerHelper.Pred: Integer;
 begin
   Result := Self - 1;
 end;
 
+// TcaInt64Helper
+
 function TcaInt64Helper.Pred: int64;
 begin
   Result := Self - 1;
 end;
 
+// TcaShiftStateHelper
+
+function TcaShiftStateHelper.ShiftToString(shift: TShiftState): string;
+var
+  EnumType: PTypeInfo;
+  EnumValue: Integer;
+  Parts: TStringList;
+begin
+  EnumType := TypeInfo(TShiftStateEnum);
+  Parts := TStringList.Create;
+  try
+    for EnumValue := 0 to Ord(High(TShiftStateEnum)) do
+    begin
+      if TShiftStateEnum(EnumValue) in Shift then
+      begin
+        Parts.Add(GetEnumName(EnumType, EnumValue));
+      end;
+    end;
+    Result := Parts.CommaText;
+  finally
+    Parts.Free;
+  end;
+end;
+
+function TcaShiftStateHelper.ToString: string;
+begin
+  Result := ShiftToString(Self);
+end;
+
 
 end.
-
